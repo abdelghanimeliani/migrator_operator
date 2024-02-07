4db2afff5c15d7c2506d13ea71d2ff5d953090a8
abdelghanimeliani <a.meliani@esi-sba.dz>
2024-02-07 14:37:03 +0100

    change the vim ip

--- controllers/delete_pod.go
+++ controllers/delete_pod.go
@@ -8,7 +8,7 @@ import (
 
 func DeletePod(namespace,podname string) (map[string]interface{}, error){
 	fmt.Println("deleting original pod")
-	baseURL := "http://172.17.54.120:8080/v1/pods"
+	baseURL := "http://172.17.13.233:8080/v1/pods"
 	urlParams := url.Values{}
 	urlParams.Set("namespace", namespace)
 	urlParams.Set("name", podname)
